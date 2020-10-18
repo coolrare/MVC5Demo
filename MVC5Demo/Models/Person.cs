2e43a730978aee57c177868a0d2aba386ba5d10f
Will <doggy.huang@gmail.com>
2020-10-18 16:43:19 +0800

    從 Model 加入 [DisplayName] 與示範單一頁面套用不同的欄位名稱

--- MVC5Demo/Models/Person.cs
+++ MVC5Demo/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,10 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
+        [DisplayName("姓名")]
         [Required(ErrorMessageResourceName = "Person_Name_Required", ErrorMessageResourceType = typeof(Resource1))]
         public string Name { get; set; }
+        [DisplayName("年齡")]
         [Required(ErrorMessage = "請輸入年紀")]
         [Range(18, 99, ErrorMessage = "請輸入年紀必須介於 {1} - {2} 歲之間")]
         public int Age { get; set; }
