0817c51a6955bc4c23c80ba816b696adeac5cb47
Will <doggy.huang@gmail.com>
2020-10-18 10:50:54 +0800

    加入 Person 模型類別與 TestController & Index View

---
+++ MVC5Demo/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = new List<Person>() {
+                new Person() { Id = 1, Name = "Will", Age = 18 },
+                new Person() { Id = 2, Name = "Tom", Age = 28 },
+                new Person() { Id = 3, Name = "Mary", Age = 38 },
+                new Person() { Id = 4, Name = "John", Age = 48 },
+            };
+
+            return View(data);
+        }
+    }
+}
