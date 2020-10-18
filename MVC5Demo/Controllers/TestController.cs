dc18d3c4f932f21f7364c340f17b6bb0127dc84b
Will <doggy.huang@gmail.com>
2020-10-18 13:28:50 +0800

    練習建立 Person 資料的「編輯」功能

--- MVC5Demo/Controllers/TestController.cs
+++ MVC5Demo/Controllers/TestController.cs
@@ -39,5 +39,25 @@ namespace MVC5Demo.Controllers
 
             return View(person);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return View(data.FirstOrDefault(p => p.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                var one = data.FirstOrDefault(p => p.Id == id);
+                one.Name = person.Name;
+                one.Age = person.Age;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
     }
 }
