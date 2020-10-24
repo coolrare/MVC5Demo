e35594337073aff7ee6728d2901677fe7584625a
Will <doggy.huang@gmail.com>
2020-10-24 10:38:16 +0800

    新增 DepartmentsController 的 Edit 動作

--- MVC5Demo/Controllers/DepartmentsController.cs
+++ MVC5Demo/Controllers/DepartmentsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
 
 namespace MVC5Demo.Controllers
@@ -38,6 +39,40 @@ namespace MVC5Demo.Controllers
             ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
 
             return View(department);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+
+            return View(db.Department.Find(id.Value));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Department.Find(id);
+
+                item.Name = department.Name;
+                item.Budget = department.Budget;
+                item.StartDate = department.StartDate;
+                item.InstructorID = department.InstructorID;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+
+            return View(db.Department.Find(id));
         }
     }
 }
