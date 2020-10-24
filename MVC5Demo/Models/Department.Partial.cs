84f1cf73c94f31a16bd491bc02eb0f11aa659dc4
Will <doggy.huang@gmail.com>
2020-10-24 15:32:26 +0800

    使用 EFGeneratePartialClass.tt 快速產生 Buddy Class 並調整 Department 的驗證屬性

--- MVC5Demo/Models/Department.Partial.cs
+++ MVC5Demo/Models/Department.Partial.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
 namespace MVC5Demo.Models
 {
-    [MetadataType(typeof(DepartmentMetadata))]
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(DepartmentMetaData))]
     public partial class Department
     {
-        public class DepartmentMetadata
-        {
-            public int DepartmentID { get; set; }
-            [Required]
-            public string Name { get; set; }
-            [Required]
-            public decimal Budget { get; set; }
-
-            [Required]
-            public Nullable<System.DateTime> StartDate { get; set; }
-            [Required]
-            public Nullable<int> InstructorID { get; set; }
-            public byte[] RowVersion { get; set; }
-        }
+    }
+    
+    public partial class DepartmentMetaData
+    {
+        [Required]
+        public int DepartmentID { get; set; }
+        
+        [Required]
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string Name { get; set; }
+        [Required]
+        public decimal Budget { get; set; }
+        [Required]
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<int> InstructorID { get; set; }
     }
 }
