fa2c550757d906b066d2a78cfbb61ad582f03d84
Will <doggy.huang@gmail.com>
2020-10-24 15:14:28 +0800

    完成 CoursesController 比較好的 Model Binding 寫法

--- MVC5Demo/Controllers/CoursesController.cs
+++ MVC5Demo/Controllers/CoursesController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Demo.Models;
+using Omu.ValueInjecter;
 
 namespace MVC5Demo.Controllers
 {
@@ -48,11 +49,14 @@ namespace MVC5Demo.Controllers
         // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CourseID,Title,Credits,DepartmentID,Memo")] Course course)
+        public ActionResult Create(CourseCreateOrEdit course)
         {
             if (ModelState.IsValid)
             {
-                db.Course.Add(course);
+                var c = db.Course.Create();
+                c.InjectFrom(course);
+
+                db.Course.Add(c);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -82,12 +86,14 @@ namespace MVC5Demo.Controllers
         // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CourseID,Title,Credits,DepartmentID,Memo")] Course course)
+        public ActionResult Edit(CourseCreateOrEdit course)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(course).State = EntityState.Modified;
+                var c = db.Course.Find(course.CourseID);
+                c.InjectFrom(course);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
             ViewBag.DepartmentID = new SelectList(db.Department, "DepartmentID", "Name", course.DepartmentID);
---
+++ MVC5Demo/Models/CourseCreateOrEdit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Demo.Models
+{
+    public class CourseCreateOrEdit
+    {
+        [Required]
+        public int CourseID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Credits { get; set; }
+        [Required]
+        public int DepartmentID { get; set; }
+        [Required]
+        public string Memo { get; set; }
+
+    }
+}
