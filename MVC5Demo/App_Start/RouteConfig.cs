8aad75f8df916ecb7ba15117461a91361d94df71
Will <doggy.huang@gmail.com>
2020-10-25 16:18:28 +0800

    示範改用 .php 為副檔名的路由

--- MVC5Demo/App_Start/RouteConfig.cs
+++ MVC5Demo/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace MVC5Demo
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}.php/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
