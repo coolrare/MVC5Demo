1db853a57edc5fde0965f216452e149e04461dfe
Will <doggy.huang@gmail.com>
2020-10-25 14:32:33 +0800

    在 DepartmentRepository 新增 "Get單一筆部門資料(int id)" 方法

--- MVC5Demo/Models/DepartmentRepository.cs
+++ MVC5Demo/Models/DepartmentRepository.cs
@@ -6,7 +6,10 @@ namespace MVC5Demo.Models
 {
 	public  class DepartmentRepository : EFRepository<Department>, IDepartmentRepository
 	{
-
+		public Department Get單一筆部門資料(int id)
+        {
+			return this.All().FirstOrDefault(p => p.DepartmentID == id);
+        }
 	}
 
 	public  interface IDepartmentRepository : IRepository<Department>
