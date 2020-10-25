98425ed7ee32ceca2e5a4e23581c78693928daad
Will <doggy.huang@gmail.com>
2020-10-25 13:46:51 +0800

    加入一段「前端驗證」範例 ( 實作 IClientValidatable 介面 )

--- MVC5Demo/Models/MustBeEvenAttribute.cs
+++ MVC5Demo/Models/MustBeEvenAttribute.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MVC5Demo.Models
 {
-    public class MustBeEvenAttribute : DataTypeAttribute
+    public class MustBeEvenAttribute : DataTypeAttribute, IClientValidatable
     {
         public MustBeEvenAttribute() : base(DataType.Text)
         {
             ErrorMessage = "請輸入偶數";
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ErrorMessage = ErrorMessageString,
+                ValidationType = "mustbeeven"
+            };
+
+            //rule.ValidationParameters["param1"] = "hello";
+            //rule.ValidationParameters["param2"] = "world";
+
+            yield return rule;
         }
 
         public override bool IsValid(object value)
