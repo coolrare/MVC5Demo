cc83723e64a288f68ef80ffc1086127a348c62cb
Will <doggy.huang@gmail.com>
2020-10-31 16:14:14 +0800

    示範 [HandlerError] 的效果，並修正預設錯誤的 Views\Shared\Error.cshtml

--- MVC5Demo/Models/Department.Partial.cs
+++ MVC5Demo/Models/Department.Partial.cs
@@ -11,6 +11,8 @@ namespace MVC5Demo.Models
         {
             if (this.Name != "Will" && this.Budget > 100)
             {
+                throw new ArgumentException("ERROR");
+
                 yield return new ValidationResult("您的預算不足", new string[] { "Budget" });
             }
         }
