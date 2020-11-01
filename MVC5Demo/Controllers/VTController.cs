0fe26036ef2ee65e4158944415f56238bd7aa15d
Will <doggy.huang@gmail.com>
2020-11-01 09:36:02 +0800

    新增空白的 VTController

---
+++ MVC5Demo/Controllers/VTController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class VTController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.data = new int[] { 1, 2, 3, 4, 5 };
+
+            return PartialView();
+        }
+    }
+}
